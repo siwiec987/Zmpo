32ef0ba53d617f29fc412b243c13fcd65e5e0f6a
Jakub Siwiec <siwiec987@gmail.com>
2025-01-09 01:02:45 +0100

    dodano możliwość użycia kamery i kilka innych zmian

--- main.py
+++ main.py
@@ -1,39 +1,98 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog, PhotoImage
+import cv2
 from recognize_from_pic import recognize_from_pic
 
 img_label = None
+video_running = False
+video = None
 
 def select_img():
+    global video_running, video, img_label
+
+    if video_running:
+        video_running = False
+        if video is not None:
+            video.release()
+        if img_label is not None:
+            img_label.destroy()
+            img_label = None
+
     path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp *.gif")])
 
     if len(path) > 0:
-        processed_img = recognize_from_pic(path)
-        
-        global img_label
-        if img_label is None:
-            img_label = ttk.Label(master = window, image=processed_img)
-            img_label.image = processed_img
-            img_label.pack()
-        else:
-            img_label.configure(image=processed_img)
-            img_label.image = processed_img
-
+        img = cv2.imread(path)
+        img = recognize_from_pic(img)
+        show_img(img)
     else:
         print("Pusto")
         return None
+    
+def show_img(processed_img):
+    global img_label
+    if img_label is None:
+        img_label = ttk.Label(master = window, image=processed_img)
+        img_label.image = processed_img
+        img_label.pack()
+    else:
+        img_label.configure(image=processed_img)
+        img_label.image = processed_img
+
+def recognize_from_vid():
+    global video_running, video, img_label
+
+    if video_running:
+        video_running = False
+        if video is not None:
+            video.release()
+        if img_label is not None:
+            img_label.destroy()
+            img_label = None
+        return
+
+    video = cv2.VideoCapture(0)
+    if not video.isOpened():
+        print("Error: Could not open video device.")
+        return None
+    
+    video_running = True
+
+    def process_frame():
+        if not video_running:
+            # video.release()
+            return
+
+        success, frame = video.read()
+        if not success:
+            print("Error: Could not read frame from video device.")
+            video.release()
+            return
+        
+        try:
+            frame = recognize_from_pic(frame)
+            show_img(frame)
+        except Exception as e:
+            print(f"Error during image processing: {e}")
+            video.release()
+            return
+        
+        window.after(10, process_frame)
+    
+    process_frame()
+
 
 window = tk.Tk()
-window.title("Pokaż ryja")
+window.iconphoto(True, PhotoImage(file="images/app-icon.png"))
+window.title("Emotion detection")
 window.geometry("1200x800")
 window.resizable(0,0)
 window.configure(bg="#2e2e2e")
 
 style = ttk.Style()
-style.theme_use("clam")  # Use a theme that supports customization
+style.theme_use("clam")
 style.configure("TFrame", background="#2e2e2e")
-style.configure("CustomTButton", background="#4d4d4d", relief="flat")
+style.configure("TButton", background="#616161", relief="flat")
 style.configure("TLabel", background="#2e2e2e")
 
 button_frame = ttk.Frame(master=window)
@@ -42,11 +101,11 @@ button_frame.pack(padx=10, pady=30)
 select_img_icon = PhotoImage(file="images/image-icon.png")
 camera_icon = PhotoImage(file="images/camera-icon.png")
 
-select_img_button = ttk.Button(master=button_frame, image=select_img_icon, command=select_img, style="CustomTButton")
+select_img_button = ttk.Button(master=button_frame, image=select_img_icon, command=select_img)
 select_img_button.image = select_img_icon
 select_img_button.pack(side=tk.LEFT, padx=15)
 
-camera_button = ttk.Button(master=button_frame, image=camera_icon, command=lambda: print("Second button clicked"))
+camera_button = ttk.Button(master=button_frame, image=camera_icon, command=recognize_from_vid)
 camera_button.image = camera_icon
 camera_button.pack(side=tk.LEFT, padx=15)
 
--- recognize_from_pic.py
+++ recognize_from_pic.py
@@ -4,8 +4,8 @@ from deepface import DeepFace
 from PIL import Image, ImageTk
 import tkinter as tk
 
-def recognize_from_pic(path):
-    img = cv2.imread(path)
+def recognize_from_pic(img):
+    # img = cv2.imread(path)
     img = resize_image(img, 600, 400)
     img = recognize_face(img)
 
@@ -26,12 +26,12 @@ def resize_image(img, width, height):
     new_width = old_width * resize_factor
     new_height = old_height * resize_factor
 
-    print("\n\nOld wymiary")
-    print(old_width)
-    print(old_height)
-    print("\n\nNew wymiary")
-    print(new_width)
-    print(new_height)
+    # print("\n\nOld wymiary")
+    # print(old_width)
+    # print(old_height)
+    # print("\n\nNew wymiary")
+    # print(new_width)
+    # print(new_height)
 
     img = cv2.resize(img, (int(new_width), int(new_height)))
 
